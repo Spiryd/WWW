eb6cbe70e0e518ccc1037ade906be798cfda1b47
Spiryd <neumann.maks@gmail.com>
2023-04-23 18:39:58 +0200

    gallery done

--- L1/gallery.js
+++ L1/gallery.js
@@ -1,25 +1,48 @@
-function loadImage(url) {
-    return new Promise(function(resolve, reject) {
-      var image = new Image();
-      image.onload = function() {
-        resolve(url);
-      };
-      image.onerror = function() {
-        reject(url);
-      };
-      image.src = url;
-    });
-  }
-  
-  var images = document.querySelectorAll('.gallery img');
-  
-  var imagePromises = Array.prototype.map.call(images, function(img) {
-    return loadImage(img.getAttribute('data-src'));
-  });
-  
-  Promise.all(imagePromises).then(function() {
-    Array.prototype.forEach.call(images, function(img) {
-      img.src = img.getAttribute('data-src');
-    });
-  });
+const galleryContainer = document.querySelector(".gallery-container");
+
+// Create an array of image sources
+const imageSources = [
+	"https://picsum.photos/id/1/300/200",
+	"https://picsum.photos/id/2/300/200",
+	"https://picsum.photos/id/3/300/200",
+	"https://picsum.photos/id/4/300/200",
+	"https://picsum.photos/id/5/300/200",
+	"https://picsum.photos/id/6/300/200",
+	"https://picsum.photos/id/7/300/200",
+	"https://picsum.photos/id/8/300/200",
+	"https://picsum.photos/id/9/300/200",
+	"https://picsum.photos/id/10/300/200",
+	"https://picsum.photos/id/11/300/200",
+	"https://picsum.photos/id/12/300/200",
+	"https://picsum.photos/id/13/300/200",
+	"https://picsum.photos/id/14/300/200",
+	"https://picsum.photos/id/15/300/200",
+	"https://picsum.photos/id/16/300/200",
+	"https://picsum.photos/id/17/300/200",
+	"https://picsum.photos/id/18/300/200",
+	"https://picsum.photos/id/19/300/200",
+	"https://picsum.photos/id/20/300/200",
+	"https://picsum.photos/id/22/300/200",
+];
+
+// Function to load each image
+function loadImage(src) {
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.onerror = reject;
+		img.src = src;
+	});
+}
+
+// Load all images using Promise.all()
+Promise.all(imageSources.map(loadImage)).then((images) => {
+	// Append each image to the gallery container
+	images.forEach((image) => {
+		const galleryItem = document.createElement("div");
+		galleryItem.classList.add("gallery-item");
+		galleryItem.appendChild(image);
+		galleryContainer.appendChild(galleryItem);
+	});
+});
   
